eb24c80b72233d469a3aa329f7a51a2173037f03
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-01 12:29:05 +0530

    code change

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,11 @@
 
 // declare class
 
-
 // export class using module.exports
+
+const Shape = require("./shape");
+class Circle extends Shape {
+  calculateArea = function () {};
+}
+
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+import React from "react";
+
+class Shape extends React.Component {
+  //color = "red";
+
+  constructor(props) {
+    super(props);
+    this.color = "red";
+  }
+  drawShape = function () {};
+  calculateArea = function () {};
+}
+
+module.exports = Shape;
